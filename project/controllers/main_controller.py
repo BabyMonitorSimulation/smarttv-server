0111881ff4c8e056acb092dbd501842fd32bdb3d
adrilene <adrilenefonseca@gmail.com>
2020-12-18 12:14:03 -0300

    new architecture

--- project/controllers/main_controller.py
+++ project/controllers/main_controller.py
@@ -6,48 +6,29 @@ from flask import request, render_template, send_from_directory, jsonify, send_f
 import json
 import requests
 
-client_tv = ClientTV()
-client_tv.subscribe()
-
+smtv = TV('locked')
 
 @app.route("/", methods=["GET"])
 def check():
     return "I'm working TV"
 
 
-@app.route("/tv_receive", methods=["POST"])
-def receive_sm():
-    global client_tv
-    if client_tv.block:
-        print("Tv's locked")
-        data = {"msg": "Tv's locked"}
-        client_tv.publish_to_dojot(data)
-        return (
-            jsonify(construct_response("status", {"info": "Tv's blocked"}, "smp")),
-            200,
-        )
-
-    else:
-        print(f"Receive {json.dumps(request.json, indent=4, sort_keys=True)}")
-        print("Tv's unlocked")
-        data = {"msg": "Tv's unlocked"}
-        client_tv.publish_to_dojot(data)
-        return (
-            jsonify(construct_response("status", {"info": "Tv's unlocked"}, "smp")),
-            200,
-        )
-
-
-@app.route("/change", methods=["POST"])
-def change():
-    global client_tv
+@app.route("/change_status", methods=["POST"])
+def change_status():
+    global smtv
     
     command = request.json["lock"]
-    client_tv.status = command
+    smtv.status = command
 
     return (
         jsonify(
-            construct_response("status", {"info": f"Tv's status is {client_tv.status}"})
+            construct_response("status", {"info": f"Tv's status is {smtv.status}"})
         ),
         200,
     )
+
+@app.route("/get_status", method=["GET"])
+def get_status():
+    global smtv
+
+    return smtv.status

c05716c7215d79ea6a8175e2c9155aacdb505476
denisousa <denismsb11@gmail.com>
2021-01-14 13:31:42 -0300

    update port

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,6 @@ from project import app, socketio
 
 
 if __name__ == "__main__":
-    print("TV Running")
-    socketio.run(app, port=5002)
+    port = 5002
+    print(f"TV Running in {port}")
+    socketio.run(app, port=port)
